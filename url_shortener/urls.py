6f63525c8d3fbb3b8f1b91fbe7adb9d41b564a11
Mahdi <mahdi57.kamali1@gmail.com>
2021-04-27 22:36:26 +0430

    Add home page view

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -4,4 +4,5 @@ from url_shortener import views
 app_name = 'url_shortener'
 urlpatterns = [
     path('u/<slug:code>/', views.short_url, name='short_url'),
+    path('', views.home, name='home'),
 ]
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'url_shortener/home.html')
+
+
 def short_url(request, code):
     url = get_object_or_404(Code, slug=code).target
     url.increase_visits()
